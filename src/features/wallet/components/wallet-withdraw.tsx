73aa84a57bb16ae450465a804a274316daebf5b2
Danu Oprea <danop-dev@users.noreply.github.com>
2023-08-21 11:34:43 +0300

    Fixed modal confirm

--- src/features/wallet/components/wallet-withdraw.tsx
+++ src/features/wallet/components/wallet-withdraw.tsx
@@ -8,7 +8,7 @@ import { DialogClose, DialogContent, DialogPortal } from "@radix-ui/react-dialog
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { useTranslation } from "react-i18next";
 import * as AlertDialog from '@radix-ui/react-alert-dialog';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import WalletWithdrawForm from "./wallet-withdraw-form";
 // import { Portal } from "@radix-ui/react-dialog";
 // import { useCallback, useState } from "react";
@@ -23,6 +23,8 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useAuth } from "@/features/auth";
 import { set } from "date-fns";
+import { walletWithdrawn } from "../api";
+import { useQueryClient } from "react-query";
 
 
 
@@ -32,41 +34,12 @@ type WalletWithdrawProps = {
 
 const WalletWithdraw = (props: WalletWithdrawProps) => {
   const { t } = useTranslation();
+  const queryClient = useQueryClient();
 
   const [openModal, setOpenModal] = useState(false);
   const [isConfirmation, setIsConfirmation] = useState(false);
+  const [isModalConfirm, setIsModalConfirm] = useState(false);
   const onClickConfirm = () => setIsConfirmation(true);
-
-  // const { balance } = props;
-
-  // const form = useForm({
-  //   resolver: useYupValidationResolver(
-  //     yup.object().shape({
-  //       amount: yup.number().min(100).max(Number(balance)).required(),
-  //     })
-  //   ),
-  // });
-  // const queryClient = useQueryClient();
-  // const dispatch = useDispatch();
-
-  // const [isWithdrawModalOpen, setIsWithdrawModalOpen] = useState(false);
-
-  // const onSubmitHandler = useCallback(
-  //   async (values) => {
-  //     try {
-  //       await api.wallet.withdrawn(values);
-  //       setIsWithdrawModalOpen(false);
-  //       queryClient.invalidateQueries(["wallet"]);
-  //       queryClient.invalidateQueries(["wallet-transactions"]);
-  //       dispatch(notification({ title: "success", descrp: "transactions.withdraw_success" }));
-  //     } catch (error) {
-  //       dispatch(
-  //         notification({ type: "error", title: "error", descrp: getApiErrorMessages(error, true) })
-  //       );
-  //     }
-  //   },
-  //   [dispatch, queryClient]
-  // );
 
   const schema = z.object({
     amount:
@@ -85,21 +58,33 @@ const WalletWithdraw = (props: WalletWithdrawProps) => {
     resolver: zodResolver(schema),
   });
 
-  const onSubmitTestIsSubmitting = (values: FormValues) => {
-    console.log(values);
+  useEffect(() => {
+    if (form.formState.isValid) form.clearErrors();
+    else form.trigger();
+  }, [form.formState.isValid]);
+
+  const onSubmitTestIsSubmitting = async (values: FormValues) => {
+    try {
+      await walletWithdrawn({
+        amount: parseInt(values.amount, 10),
+      });
+      await Promise.allSettled([
+        queryClient.invalidateQueries(["wallet"]),
+        queryClient.invalidateQueries(["historyTransaction"]),
+      ]);
+      setIsModalConfirm(false);
+      setOpenModal(false);
+      form.reset();
+    } catch (err) {
+      console.log(err);
+    }
   };
 
-  const onConfirm = () => {
+  const onApply = () => {
     setIsConfirmation(false);
     if (form.formState.isValid) {
-      // If form is valid, open the confirmation dialog
-      console.log("form.formState.isValid", form.formState.isValid);
-      
+      setIsModalConfirm(true);
       setIsConfirmation(true);
-    } else {
-      console.log("form.formState.isValid2", form.formState.isValid);
-      // Otherwise, trigger a validation (this will show validation errors on the form)
-      form.trigger();
     }
   };
 
@@ -138,14 +123,16 @@ const WalletWithdraw = (props: WalletWithdrawProps) => {
               </DialogClose>
 
               {/* Dialog Confirm */}
-              <AlertDialog.Root>
+              <AlertDialog.Root
+                open={isModalConfirm}
+                onOpenChange={setIsModalConfirm}
+              >
                 {/* <AlertDialog.Trigger asChild> */}
-                  <Button
-                    onClick={onConfirm}
-                    className="text-white inline-flex items-center justify-center rounded-[4px] px-4 font-medium leading-none focus:shadow-[0_0_0_2px] focus:outline-none bg-primary hover:bg-primary-hover xs:hover:bg-primary-hover sm:hover:bg-primary-hover md:hover:bg-primary-hover">
-                    {t("common:apply")}
-                  </Button>
-                {/* </AlertDialog.Trigger> */}
+                <Button
+                  onClick={onApply}
+                  className="text-white inline-flex items-center justify-center rounded-[4px] px-4 font-medium leading-none focus:shadow-[0_0_0_2px] focus:outline-none bg-primary hover:bg-primary-hover xs:hover:bg-primary-hover sm:hover:bg-primary-hover md:hover:bg-primary-hover">
+                  {t("common:apply")}
+                </Button>
                 <AlertDialog.Portal>
                   <AlertDialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[500px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
                     <AlertDialog.Title className="text-gray-900 mb-2 text-[17px] font-medium">
@@ -166,7 +153,7 @@ const WalletWithdraw = (props: WalletWithdrawProps) => {
                         <button
                           onClick={() => {
                             onClickConfirm();
-                            form.handleSubmit(onSubmitTestIsSubmitting)(); // Trigger the submission
+                            form.handleSubmit(onSubmitTestIsSubmitting)();
                           }}
                           className=" px-3 py-2 text-red-400 hover:bg-red-100 rounded-[4px] px-[15px] font-medium outline-none">
                           {t("common:confirm")}
