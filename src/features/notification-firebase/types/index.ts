eea9819f1b74ba0513b2503ef6ebf880248751c1
anDarya <dary.andronic.office@gmail.com>
2023-09-06 17:18:15 +0300

    Create a conection with firebase and send token on apiFCMToken

---
+++ src/features/notification-firebase/types/index.ts
@@ -0,0 +1,11 @@
+export type firebaseConfigTypes = {
+  authDomain: string;
+  storageBucket: string;
+  apiKey: string;
+  projectId: string;
+  messagingSenderId: string;
+  appId: string;
+};
+export interface tokenOptions {
+  vapidKey: string;
+}
