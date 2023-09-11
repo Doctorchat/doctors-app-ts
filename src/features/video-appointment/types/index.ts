50e9becf3f7b294515eefcab36e4a657dc76bcca
Danu Oprea <danop-dev@users.noreply.github.com>
2023-09-11 23:31:33 +0300

    [DOC-850] - Add Appoiment type

--- src/features/video-appointment/types/index.ts
+++ src/features/video-appointment/types/index.ts
@@ -9,3 +9,22 @@ export type SetDisponibilityPayload = {
   time_frame: number;
   time_buffer: number;
 } & Disponibility;
+
+export type Appointment = {
+  card_id: null | string;
+  chat_id: number;
+  created_at: string;
+  doctor_id: number;
+  doctor_notified: number;
+  doctor_notified_in_one_day: number;
+  end_time: string;
+  expired_at: null | string;
+  id: number;
+  payment_id: null | string;
+  start_time: string;
+  status: string;
+  updated_at: string;
+  user_id: number;
+  user_notified: number;
+  user_notified_in_one_day: number;
+};
