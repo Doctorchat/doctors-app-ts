9de9a8a3c26b9416570af40b7fb1c654c2e5ddaf
Anton Gadimba <antonhck@gmail.com>
2023-11-03 14:17:57 +0200

    Another method to use env

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,16 +4,14 @@ importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
 // eslint-disable-next-line no-undef
 importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js");
 
-import process from "node:process";
-
 // Initialize the Firebase app in the service worker by passing the generated config
 var firebaseConfig = {
-  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  apiKey: `${process.env.VITE_APP_API_KEY}`,
   authDomain: "doctorchat-push.firebaseapp.com",
   projectId: "doctorchat-push",
   storageBucket: "doctorchat-push.appspot.com",
-  messagingSenderId: process.env.VITE_FIREBASE_SENDER_ID,
-  appId: process.env.VITE_FIREBASE_APP_ID,
+  messagingSenderId: `${process.env.VITE_APP_FIREBASE_API_KEY}`,
+  appId: `${process.env.VITE_FIREBASE_APP_ID}`,
 };
 // eslint-disable-next-line no-undef
 firebase.initializeApp(firebaseConfig);
