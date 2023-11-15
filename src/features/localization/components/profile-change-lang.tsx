abf0a68c40673112433d1eea628dfc1b5b2eabc3
Andronic Daria <dary.andronic.office@gmail.com>
2023-11-15 15:28:21 +0200

    Doc 1003 fe adaugarea dropdown pentru traduceri (#44)
    * Adding a flag of languages
    
    * Adding a short version of language on page of login
    
    * Remove logs
    
    * Adding a new image for ro language
    
    * Adding ro
    
    * delete ro1
    
    * Delete ro
    
    * Adding a png
    
    * Rename file
    
    * Adding a default language without cache

--- src/features/localization/components/profile-change-lang.tsx
+++ src/features/localization/components/profile-change-lang.tsx
@@ -1,7 +1,4 @@
-import React, { useCallback } from "react";
-
-import { LanguageIcon } from "@heroicons/react/24/outline";
-
+import React, { useCallback, useEffect } from "react";
 import { Button } from "@/components/ui";
 import {
   DropdownMenu,
@@ -13,9 +10,10 @@ import { useAppI18n } from "@/hooks";
 import { AppLocale } from "@/types";
 import { setUserLocale } from "../api";
 import i18n from "@/lib/i18n";
+import { Avatar } from "antd";
 
 export interface ProfileChangeLangProps {
-  disabled?: boolean;
+  isShortText?: boolean;
 }
 
 const langs: { [index: string]: string } = {
@@ -24,28 +22,53 @@ const langs: { [index: string]: string } = {
   en: "English",
 };
 
-const ProfileChangeLang: React.FC<ProfileChangeLangProps> = ({ disabled }) => {
+const langsShort: { [index: string]: string } = {
+  ro: "RO",
+  ru: "RU",
+  en: "EN",
+};
+
+const ProfileChangeLang: React.FC<ProfileChangeLangProps> = ({ isShortText = false }) => {
   const { setLanguage } = useAppI18n();
 
+  useEffect(() => {
+    if (i18n.language === "en-US") setLanguage("en" as AppLocale);
+  }, [i18n.language]);
+
   const changeLanguage = useCallback((lng: string) => {
+    if (isShortText) {
+      localStorage.setItem("i18nextLng", lng);
+      setLanguage(lng as AppLocale);
+      return;
+    }
     setUserLocale(lng);
     localStorage.setItem("i18nextLng", lng);
     setLanguage(lng as AppLocale);
   }, []);
 
   return (
-    <div className="mr-2 ">
+    <div className="mr-2">
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button className="select-none" variant="ghost">
-            <LanguageIcon className="h-5 w-5" /> &nbsp;
-            {langs[i18n.language]}
+          <Button className="select-none px-2" variant="ghost">
+            <Avatar
+              size={18}
+              shape="circle"
+              src={`/img/countries/${i18n.language}.png`}
+              className="mr-2"
+            />
+            {isShortText ? langsShort[i18n.language] : langs[i18n.language]}
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent side="top" align="start">
           {Object.keys(langs).map((lng) => (
-            <DropdownMenuItem key={lng} onClick={() => changeLanguage(lng)}>
-              {langs[lng]}
+            <DropdownMenuItem
+              className="flex items-center justify-evenly"
+              key={lng}
+              onClick={() => changeLanguage(lng)}
+            >
+              <Avatar size={18} shape="circle" src={`/img/countries/${lng}.png`} />
+              {isShortText ? langsShort[lng] : langs[lng]}
             </DropdownMenuItem>
           ))}
         </DropdownMenuContent>
